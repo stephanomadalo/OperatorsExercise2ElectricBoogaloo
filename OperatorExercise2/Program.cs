c5769f06f961057020f331fd136d78d40c96b57b
Stephen Madalo <stephenmadalo89@gmail.com>
2022-04-18 21:34:46 -0400

    initial commit

---
+++ OperatorExercise2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OperatorExercise2 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the radius of your circle ?");
+            var radius = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Area is {AreaOfCircle(radius)}");
+        }
+        public static double AreaOfCircle(double radius) 
+        {
+            var area = (Math.PI * radius * radius);
+            return area;
+        }
+    }
+}
+
+
